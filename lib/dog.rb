946ad7b4aa030fc09056df5362301434cd4c6ddb
Jane Kim <jane.kim0717@gmail.com>
2022-03-14 23:16:51 -0400

    commit tree 99bfab3aad034987838e7be59df6bf082a9de97d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Dog
 
+    def name=(name)
+        @name = name
+    end 
+
+    def name 
+        @name 
+    end
+
+    def breed=(breed)
+        @breed=breed
+    end 
+
+    def breed 
+        @breed
+    end
+
 end
+
+indy = Dog.new
+
+binding.pry
+0
